c2697e4dd552890dc26833f7fadff9d421c77e4a
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2023-08-04 18:04:40 +0300

    fix header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,15 @@
-import { Container } from 'components/common/Container';
+// import { Container } from 'components/common/Container';
 import * as SC from './Header.styled';
-import { HeaderMobile } from './HeaderMobile';
+// import { HeaderMobile } from './HeaderMobile';
+// import { HeaderStandart } from './HeaderStandart';
 import { useMediaQuery } from '@yamada-ui/use-media-query';
-import { HeaderStandart } from './HeaderStandart';
 
 export const Header = () => {
   const [tablet] = useMediaQuery('(min-width: 768px)');
 
   return (
     <SC.Header>
-      <Container>{tablet ? <HeaderStandart /> : <HeaderMobile />}</Container>
+      {/* <Container>{tablet ? <HeaderStandart /> : <HeaderMobile />}</Container> */}
     </SC.Header>
   );
 };
