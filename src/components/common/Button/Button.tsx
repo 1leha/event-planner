965b27e43d15da9f13d93177337c489545d9ed51
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2023-08-06 23:26:08 +0300

    fix button props

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -2,11 +2,24 @@ import { useMediaQuery } from '@yamada-ui/use-media-query';
 import * as SC from './Button.styled';
 import { IButton } from './interface';
 
-export const Button = ({ children, size, variant, width, icon }: IButton) => {
+export const Button = ({
+  children,
+  size,
+  variant,
+  width,
+  icon,
+  ...restProps
+}: IButton) => {
   const [mobile] = useMediaQuery('(max-width: 767px)');
 
   return (
-    <SC.Button size={size} width={width} variant={variant} icon={icon}>
+    <SC.Button
+      size={size}
+      width={width}
+      variant={variant}
+      icon={icon}
+      {...restProps}
+    >
       <SC.Wrapper>
         {icon && <SC.Icon>{icon}</SC.Icon>}
         {(!mobile && children) || (!icon && children)}
