e9484dea515801acffcda52870ce17e5e75daf97
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2023-08-16 07:41:16 +0300

    adding regExp and form payload

--- src/helpers/validation/formInputSchema.ts
+++ src/helpers/validation/formInputSchema.ts
@@ -2,13 +2,26 @@ import * as yup from 'yup';
 import { InferType } from 'yup';
 
 export const formInputSchema = yup.object({
-  title: yup.string().required('This field is required'),
+  title: yup
+    .string()
+    .required('This field is required')
+    .matches(/^(?![,.\-' ])[a-zA-Zа-яА-Яіїєґ',.\- 0-9]*$/u, 'Wrong input!'),
+
   description: yup.string().min(5, 'Minimum 5 char').max(80, 'Max 80 char'),
+
   date: yup.string().required('This field is required'),
+
   time: yup.string().required('This field is required'),
-  location: yup.string().required('This field is required'),
+
+  location: yup
+    .string()
+    .required('This field is required')
+    .matches(/^(?![,.\-' ])[a-zA-Zа-яА-Яіїєґ',.\- 0-9]*$/u, 'Wrong input!'),
+
   category: yup.string().required('This field is required'),
+
   image: yup.string(),
+
   priority: yup.string().required('This field is required'),
 });
 
