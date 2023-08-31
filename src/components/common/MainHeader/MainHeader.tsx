2a697382114b334871106d98402fbf9f208718d5
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2023-08-31 07:43:05 +0300

    fix styles, adding navigate to create

--- src/components/common/MainHeader/MainHeader.tsx
+++ src/components/common/MainHeader/MainHeader.tsx
@@ -5,9 +5,11 @@ import { useMediaQuery } from '@yamada-ui/use-media-query';
 import { DropDownMenu } from '../DropDownMenu';
 import { CategoryDropDown } from 'components/CategoryDropDown';
 import { SortByDropDown } from 'components/SortByDropDown';
+import { useNavigate } from 'react-router-dom';
 
 export const MainHeader = () => {
   const [tablet] = useMediaQuery(['(min-width: 768px)', '(max-width: 1320px)']);
+  const navigate = useNavigate();
 
   return (
     <SC.Wrapper>
@@ -16,7 +18,12 @@ export const MainHeader = () => {
 
         <SortByDropDown />
 
-        <Button variant="primary" size="l" icon={<SVG.PlusIcon />}>
+        <Button
+          variant="primary"
+          size="l"
+          icon={<SVG.PlusIcon />}
+          onClick={() => navigate('create')}
+        >
           Add new event
         </Button>
       </SC.ButtonsWrapper>
