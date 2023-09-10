1952edecc9c2b42cdf61e8604f9a41ce8f3f19c7
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2023-09-10 15:45:29 +0300

    fix app background styles

--- src/components/Main/Main.styled.ts
+++ src/components/Main/Main.styled.ts
@@ -1,8 +1,11 @@
 import { styled } from 'styled-components';
 import appBG from 'img/icons/bg.svg';
+import { breakPoint } from 'settings/breakpoints';
 
 export const Main = styled.main`
   box-sizing: border-box;
+
+  min-height: calc(100vh - 168px);
 
   padding-top: ${p => p.theme.space[13]}px;
   padding-bottom: ${p => p.theme.space[13]}px;
@@ -10,4 +13,8 @@ export const Main = styled.main`
   background-image: url(${appBG});
   background-repeat: no-repeat;
   background-size: cover;
+
+  @media screen and (min-width: ${breakPoint.tablet}px) {
+    min-height: calc(100vh - 92px);
+  }
 `;
